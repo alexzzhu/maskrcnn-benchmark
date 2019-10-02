baa5cbec3cec5f7b81dca5782d04746506f995e9
alexzzhu <alex.z.zhu@gmail.com>
2019-10-01 22:40:05 -0400

    Fixed bug in model loading where weights were never loaded

--- maskrcnn_benchmark/utils/model_serialization.py
+++ maskrcnn_benchmark/utils/model_serialization.py
@@ -37,27 +37,27 @@ def align_and_update_state_dicts(model_state_dict, loaded_state_dict):
     idxs[max_match_size == 0] = -1
 
     # used for logging
-    """
+    
     max_size = max([len(key) for key in current_keys]) if current_keys else 1
     max_size_loaded = max([len(key) for key in loaded_keys]) if loaded_keys else 1
-    log_str_template = "{: <{}} loaded from {: <{}} of shape {}"
-    logger = logging.getLogger(__name__)
+    #log_str_template = "{: <{}} loaded from {: <{}} of shape {}"
+    #logger = logging.getLogger(__name__)
     for idx_new, idx_old in enumerate(idxs.tolist()):
         if idx_old == -1:
             continue
         key = current_keys[idx_new]
         key_old = loaded_keys[idx_old]
         model_state_dict[key] = loaded_state_dict[key_old]
-        logger.info(
-            log_str_template.format(
-                key,
-                max_size,
-                key_old,
-                max_size_loaded,
-                tuple(loaded_state_dict[key_old].shape),
-            )
-        )
-    """
+        #logger.info(
+        #    log_str_template.format(
+        #        key,
+        #        max_size,
+        #        key_old,
+        #        max_size_loaded,
+        #        tuple(loaded_state_dict[key_old].shape),
+        #    )
+        #)
+    
 
 def strip_prefix_if_present(state_dict, prefix):
     keys = sorted(state_dict.keys())
@@ -114,5 +114,9 @@ def load_state_dict(model, loaded_state_dict):
                 torch.nn.init.zeros_(var)
             elif 'bn1.running_var' in key:
                 torch.nn.init.ones_(var)
+        strict = False
+    else:
+        strict = True
+        print("Prev weights found, using these")
     # use strict loading
-    model.load_state_dict(model_state_dict, strict=False)
+    model.load_state_dict(model_state_dict, strict=strict)
